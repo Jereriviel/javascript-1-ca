e92700192b56ebced4a71a387131b54305230fec
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2025-03-08 11:52:39 +0100

    Fixed display price and total for mobile view.

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -40,70 +40,93 @@ async function removeFromCart(productId) {
 
 function displayProducts(products) {
   var topSummary = document.querySelector(".top-summary");
+  var mobileSummary = document.querySelector(
+    ".order-summary__total-mobile .mobile-price"
+  );
+
+  mobileSummary.innerHTML = "";
 
   for (var i = 0; i < products.length; i++) {
     var product = products[i];
 
     topSummary.innerHTML += `
-<div class="order-card" data-id="${product.id}">
-  <img
-    class="order-summary__image"
-    src="${product.image.url}"
-    alt="${product.image.alt}"
-  />
-  <div class="product-card">
-    <div class="p-text-trash">
-      <div class="p-text">
-        <p>${
-          product.gender === "Male"
-            ? "Men's"
-            : product.gender === "Female"
-            ? "Women's"
-            : product.gender
-        }</p>
+    <div class="order-card__summary" data-id="${product.id}">
+        <img
+          class="order-summary__image"
+          src="${product.image.url}"
+          alt="${product.image.alt}"
+        />
+      <div class="product-card__summary">
+        <div class="product-card__top-trash">
+          <div class="p-text">
+              <p>${
+                product.gender === "Male"
+                  ? "Men's"
+                  : product.gender === "Female"
+                  ? "Women's"
+                  : product.gender
+              }</p>
+              <p class="product-card__name">${product.title}</p>
+              <p><span>Size Medium</span></p>
+          </div>
+            
+              <div class="p-trash" onclick="removeFromCart('${product.id}')">
+                <span class="material-symbols-outlined"> delete </span>
+              </div>
+            
+        </div>
+          <div class="p-number-price">
+          <p class="valuta">$</p>
+            <div class="p-price">
+              <p class="order-summary__item-price">${product.discountedPrice.toFixed(
+                2
+              )}</p>
+            </div>
+          </div>
+      </div>
+    </div>
+    `;
+
+    mobileSummary.innerHTML += `
+      <div class="product-item">
+      <div class="product-item__header">
+      <p>${
+        product.gender === "Male"
+          ? "Men's"
+          : product.gender === "Female"
+          ? "Women's"
+          : product.gender
+      }</p>
         <p class="product-card__name">${product.title}</p>
-        <p>
-          <span>Size Medium</span>
-        </p>
-      </div>
-      <div class="p-trash" onclick="removeFromCart('${product.id}')">
-        <span class="material-symbols-outlined"> delete </span>
-      </div>
-    </div>
-    <div class="p-number-price">
-      <div class="p-price">
-        <p class="order-summary__item-price">${product.discountedPrice}</p>
-      </div>
-    </div>
-  </div>
-</div>
-      `;
+        </div>
+        <div class="valuta-and-price">
+        <p class="valuta">$</p>
+        <p class="order-summary__item-price">${product.discountedPrice.toFixed(
+          2
+        )}</p>
+        </div>
+        </div>
+    `;
   }
+
+  displayTotal();
 }
 
-function displayTotal(products) {
+function displayTotal() {
   var itemTotalElement = document.querySelector(".order-summary__item-total");
+  var mobileTotalElement = document.querySelector(
+    ".order-summary__total-mobile .order-summary__item-total"
+  );
 
   var priceElements = [
-    ...document.querySelectorAll(".order-summary__item-price"),
+    ...document.querySelectorAll(".top-summary .order-summary__item-price"),
   ];
 
-  var total = priceElements.reduce((acc, product) => {
-    return acc + Number(product.textContent);
-  }, 0);
+  var total = priceElements.reduce(
+    (acc, product) => acc + Number(product.textContent),
+    0
+  );
 
   itemTotalElement.textContent = total.toFixed(2);
-}
-
-function checkoutEmptyCart() {
-  var checkoutButton = document.querySelector("#checkout-button");
-
-  checkoutButton.addEventListener("click", clearCart);
-}
-
-checkoutEmptyCart();
-
-function clearCart() {
-  window.sessionStorage.setItem("cart", JSON.stringify([]));
-  cartNotification();
+  mobileTotalElement.textContent = total.toFixed(2);
 }
