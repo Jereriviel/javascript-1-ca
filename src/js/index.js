30742e552ca520d4875fda696d9aa30f4e990512
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2025-03-09 22:19:04 +0100

    Minor edits to styling.

--- src/js/index.js
+++ src/js/index.js
@@ -27,41 +27,46 @@ function displayProducts(products) {
 
     productNewest.innerHTML += `
       <article class="product-card">
-      <figure class="product-card__image-container product-card__zoom">
-      <a href="./products/product.html?id=${product.id}">
-            <img
-              class="product-card__image"
-              src="${product.image.url}"
-              alt="${product.image.alt}"
-            />
-            </a>
-          </figure>
-          <div class="product-card__details">
-              <div class="product-card__top">
-                <div class="product-card__heading">
-                  <p class="product-card__gender">${
-                    product.gender === "Male"
-                      ? "Men's"
-                      : product.gender === "Female"
-                      ? "Women's"
-                      : product.gender
-                  }</p>
-  <a href="./products/product.html?id=${product.id}">
-                  <p class="product-card__name">${product.title}</p>
-  </a>
-                </div>
-                <span class="material-symbols-outlined${
-                  product.favorite ? " filled" : ""
-                }"> favorite </span>
-              </div>
-              <div class="product-card__bottom">
-                <p class="product-card__price">$ ${product.discountedPrice}</p>
-                <p class="product-card__before-price">${
-                  product.price !== product.discountedPrice ? product.price : ""
+    <figure class="product-card__image-container product-card__zoom">
+    <a href="./products/product.html?id=${product.id}">
+          <img
+            class="product-card__image"
+            src="${product.image.url}"
+            alt="${product.image.alt}"
+          />
+          ${
+            product.price !== product.discountedPrice
+              ? '<div class="on-sale"><p>On sale</p></div>'
+              : ""
+          }
+          </a>
+        </figure>
+        <div class="product-card__details">
+            <div class="product-card__top">
+              <div class="product-card__heading">
+                <p class="product-card__gender">${
+                  product.gender === "Male"
+                    ? "Men's"
+                    : product.gender === "Female"
+                    ? "Women's"
+                    : product.gender
                 }</p>
+<a href="./products/product.html?id=${product.id}">
+                <p class="product-card__name">${product.title}</p>
+</a>
               </div>
+              <span class="material-symbols-outlined${
+                product.favorite ? " filled" : ""
+              }"> favorite </span>
             </div>
-        </article>
+            <div class="product-card__bottom">
+              <p class="product-card__price">$ ${product.discountedPrice}</p>
+<p class="product-card__before-price">
+  ${product.price !== product.discountedPrice ? `$ ${product.price}` : ""}
+</p>
+            </div>
+          </div>
+      </article>
       
       `;
   }
